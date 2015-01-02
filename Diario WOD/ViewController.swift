af3bf0a6f5695c9e3b78cb8ce6a56c62cb617854
Tiago Sandes Fernandes <tsandesfernandes@gmail.com>
2015-01-02 09:04:53 -0200

    Initial Commit

---
+++ Diario WOD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diario WOD
+//
+//  Created by Tiago Sandes Fernandes on 02/01/15.
+//  Copyright (c) 2015 Tiago Sandes Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
